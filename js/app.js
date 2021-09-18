3b4d0d347728eeb8e0268432ce1999394a6354d4
vitalydok <89067055+vitalydok@users.noreply.github.com>
2021-09-18 22:09:39 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -56,7 +56,7 @@ function finishGame() {
 
 function createRandomCircle() {
     const circle = document.createElement('div')
-    const size = getRandomNumber(10, 30)
+    const size = getRandomNumber(10, 50)
     const {width, height} = board.getBoundingClientRect()
     const x = getRandomNumber(0, width - size)
     const y = getRandomNumber(0, height - size)
@@ -66,7 +66,7 @@ function createRandomCircle() {
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
-    circle.style.backgroundColor = setColor(circle)
+    circle.style.background = setColor(circle)
 
     
     board.append(circle)
